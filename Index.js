bed0e70addd490b9f8380aa60fff4d5d1ac1b033
Alex Karales <karales@gmail.com>
2024-03-15 11:56:58 -0400

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Simulation</title>
+</head>
+<body>
+  <ul id="output-stream-list"></ul>
+  <div id="status"></div>
+  <script src="contentScript.js"></script>
+  <script src="background.js"></script>
+</body>
+</html>
